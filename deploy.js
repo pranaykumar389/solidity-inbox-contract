4d73ec7edcc873285f4a160c050fce649e1e558a
Pranay Kumar <pranaykumar389@users.noreply.github.com>
2021-11-16 04:23:24 +0000

    chore(minor): upgrade solc v0.8.9

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+
+const { abi, evm } = require('./compile');
 
 const mySecret = process.env.MNEMONIC;
 const rinkebyApi = process.env.RINKEBY_API;
@@ -9,6 +10,7 @@ const provider = new HDWalletProvider(
   mySecret,
   rinkebyApi
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -17,11 +19,11 @@ const deploy = async () => {
 
     console.log('Attempting to deploy from contract', accounts[0]);
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: ['Hello Inbox Contract'] })
+    const result = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object, arguments: ['Hello Inbox Contract'] })
       .send({ from: accounts[0], gas: '1000000' });
 
-    console.log('Contract deployed to', result);
+    console.log('Contract deployed to', result.options.address);
   } catch (error) {
     console.error(error);
   }
--- test/Inbox.spec.js
+++ test/Inbox.spec.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+
+const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
@@ -12,8 +13,8 @@ beforeEach(async() => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
   // Use one of those accounts to deploy contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-  .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+  inbox = await new web3.eth.Contract(abi)
+  .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
   .send({ from: accounts[0], gas: '1000000' });
 });
 
